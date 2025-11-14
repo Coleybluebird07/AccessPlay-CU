faf6fc63d7fab7a11135fd99d8d2be61436e0b5b
David Cole <coled4@cardiff.ac.uk>
2025-11-14 10:54:32 +0000

    Edit schema.sql removing useless comments

--- auth-server/schema.sql
+++ auth-server/schema.sql
@@ -1,11 +1,6 @@
--- Run this in your MariaDB client
 CREATE DATABASE IF NOT EXISTS `group7_auth` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `group7_auth`;
 
--- Create a dedicated user (optional; or use root in dev)
--- CREATE USER 'group7_user'@'%' IDENTIFIED BY 'strong_password_here';
--- GRANT ALL PRIVILEGES ON group7_auth.* TO 'group7_user'@'%';
--- FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS users (
   id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -15,6 +10,3 @@ CREATE TABLE IF NOT EXISTS users (
   PRIMARY KEY (id),
   INDEX (email)
 );
-
--- Sanity check
--- SELECT * FROM users;
