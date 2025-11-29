8ece0f3b60ad824ab2af50b0a5fe7005bc779a97
Francis Yates <yatesf1@cardiff.ac.uk>
2025-11-29 10:26:26 +0000

    working microphone. added comments to my previous code

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-
 import LandingPage from './LandingPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/LoginPage/ResgisterPage';
 import Browse_games from './components/browser_games/browse_game';
+import MicrophoneButton from './components/MicrophoneButton';
 import './landing-page.css';
 import './navbar.css';
 import './components/browser_games/browse_game.css';
@@ -14,24 +14,25 @@ export default function App() {
 
   return (
     <>
-    <div className="navbar">
-      <a href="/" className="nav-logo">AccessPlay</a>
+      <div className="navbar">
+        <a href="/" className="nav-logo">AccessPlay</a>
 
-      <div className="nav-links">
-        <a href="/">Home</a>
-        <a href="/browse-games">Browse Games</a>
-        <a href="/login">Login</a>
-        <a href="/register" className="nav-button">Register</a>
+        <div className="nav-links">
+          <a href="/">Home</a>
+          <a href="/browse-games">Browse Games</a>
+          <a href="/login">Login</a>
+          <a href="/register" className="nav-button">Register</a>
+        </div>
       </div>
-    </div>
 
-    <div className= "page-wrapper">
-      {path === "/register" && <RegisterPage />}
-      {path === "/login" && <LoginPage />}
-      {path === "/" && <LandingPage />}
-      {path === "/browse-games" && <Browse_games />}
-    </div>
+      <div className="page-wrapper">
+        {path === "/register" && <RegisterPage />}
+        {path === "/login" && <LoginPage />}
+        {path === "/" && <LandingPage />}
+        {path === "/browse-games" && <Browse_games />}
+      </div>
 
-  </>
- );
+      <MicrophoneButton />
+    </>
+  );
 }
